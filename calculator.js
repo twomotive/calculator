91a67b62c21e180d7b41154c12177308bf0302d7
twomotive <112391727+twomotive@users.noreply.github.com>
2023-09-12 14:00:05 +0300

    finished calculator.js added css effects

--- calculator.js
+++ calculator.js
@@ -1,4 +1,110 @@
-const sum = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const mul = (a, b) => a * b;
-const div = (a, b) => a / b;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = "";
+let operatorCount = 0;
+
+function resetNums() {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  result = "";
+  operatorCount = 0;
+}
+
+const clear = document.querySelector("#clear");
+const equal = document.querySelector("#equal");
+const sum = document.querySelector("#sum");
+const sub = document.querySelector("#sub");
+const mul = document.querySelector("#mul");
+const div = document.querySelector("#div");
+const dot = document.querySelector("#dot");
+const resultShow = document.querySelector("#result");
+const operatorButtons = document.querySelectorAll(".pros");
+const numbers = document.querySelectorAll(".btnum");
+const calscreen = document.querySelector("#histext");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (operatorCount === 0) {
+      num1 += number.textContent;
+      resultShow.textContent = num1;
+    } else if (operatorCount === 1) {
+      num2 += number.textContent;
+      resultShow.textContent = num2;
+    } else {
+      num2 += number.textContent;
+      resultShow.textContent = num2;
+    }
+  });
+});
+
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    operatorCount++;
+    if (operatorCount == 2) {
+      firstCalculate();
+
+      num2 = "";
+    } else if (operatorCount > 2) {
+      nextCalculate();
+
+      num2 = "";
+    }
+
+    operator = operatorButton.textContent;
+    if (operatorCount === 1) {
+      calscreen.textContent = num1 + " " + operator;
+    } else {
+      calscreen.textContent = parseFloat(result).toFixed(1) + " " + operator;
+    }
+  });
+});
+
+function firstCalculate() {
+  if (operator === "+") {
+    result = parseFloat(num1) + parseFloat(num2);
+  } else if (operator === "-") {
+    result = parseFloat(num1) - parseFloat(num2);
+  } else if (operator === "*") {
+    result = parseFloat(num1) * parseFloat(num2);
+  } else if (operator === "/") {
+    result = parseFloat(num1) / parseFloat(num2);
+  }
+  resultShow.textContent = parseFloat(result).toFixed(1);
+}
+
+function nextCalculate() {
+  switch (operator) {
+    case "+":
+      result = parseFloat(result) + parseFloat(num2);
+      break;
+    case "-":
+      result = parseFloat(result) - parseFloat(num2);
+      break;
+    case "*":
+      result = parseFloat(result) * parseFloat(num2);
+      break;
+    case "/":
+      result = parseFloat(result) / parseFloat(num2);
+      break;
+  }
+  resultShow.textContent = parseFloat(result).toFixed(1);
+}
+
+equal.addEventListener("click", () => {
+  calscreen.textContent = "";
+  if (operatorCount === 1) {
+    firstCalculate();
+    resetNums();
+  } else if (operatorCount > 1) {
+    nextCalculate();
+    resetNums();
+  }
+});
+
+clear.addEventListener("click", () => {
+  resetNums();
+  resultShow.textContent = "";
+  calscreen.textContent = "";
+});
